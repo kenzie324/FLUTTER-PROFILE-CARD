231d9542048313252b0e5cbbd64b31e3ce5ce2e8
kenzie324 <Kenzie.gantheng@gmail.com>
2025-10-23 13:53:35 +0700

    belajar container, padding, sizedbox

--- lib/profile.dart
+++ lib/profile.dart
@@ -8,35 +8,54 @@ class ProfileCard extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.redAccent,
-        title: const Text('My profile', style: TextStyle(color: Colors.white,
-        fontWeight: FontWeight.bold,
-        ),
+        title: const Text(
+          'My profile',
+          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
         ),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text('Muhammad Kenzie Arzachel',
-            style: TextStyle(
-              fontSize: 20,
-              fontWeight: FontWeight.bold,
-              
+            Container(
+              width: MediaQuery.of(context).size.width * 0.8 ,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                color: Colors.amber,
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.redAccent,
+                    spreadRadius: 5.0,
+                    blurRadius: 7.0,
+                    offset: Offset(0, 3),
+                  )
+                ]
+              ),
+              height: 300,
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 20,
+                      vertical: 50,
+                      ),
+                    child: Text('NAME',
+                    style:  TextStyle(
+                      fontSize: 24, fontWeight: FontWeight.bold),),
+                  ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                  Text(
+                    'Muhammad Kenzie Arzachel',
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
+                  ),
+                ],
+              ),
             ),
-            ),
-            Row(
-              children: <Widget>[
-                Column(
-                  children: <Widget>[],
-                ),
-                Column(
-                  children: <Widget>[],
-                ),
-              ],
-            )
           ],
         ),
       ),
     );
-}
+  }
 }
